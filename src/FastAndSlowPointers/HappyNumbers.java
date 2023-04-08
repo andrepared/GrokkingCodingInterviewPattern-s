fe495db478e68197853d51632f22f854a4b24032
dev <50889119+andrepared@users.noreply.github.com>
2023-04-08 13:51:47 -0500

    Happy Number first push

--- src/FastAndSlowPointers/HappyNumbers.java
+++ src/FastAndSlowPointers/HappyNumbers.java
@@ -2,5 +2,21 @@ package FastAndSlowPointers;
 
 public class HappyNumbers
 {
+public static String printStingWithMarkers(String strn, int pValue)
+{
+    String out = "";
+    for (int i = 0; i < pValue; i++) {
+        out += String.valueOf(strn.charAt(i));
+    }
+    out += "«";
+    out += String.valueOf(strn.charAt(pValue)) + "»";
+    for (int i = pValue + 1; i < strn.length(); i++) {
+        out += String.valueOf(strn.charAt(i));
+    }
+    return out;
+}
+public static int sumOfSquaredDigits(int number)
+{
 
+}
 }
