9c9e13127dadf107030443f67198bc47d609efe5
dev <50889119+andrepared@users.noreply.github.com>
2023-04-08 12:51:34 -0500

    reverse string

--- src/TwoPointers/ReverseWords.java
+++ src/TwoPointers/ReverseWords.java
@@ -3,59 +3,33 @@ import java.util.*;
 import java.lang.String;
 class ReverseWords {
 
-//    public static String reverseWords(String s) {
-//        // trim spaces and convert string to string builder
-//        char[] s1 = s.toCharArray();
-//        String s2 = cleanSpaces(s1, s1.length);
-//        StringBuilder builder = new StringBuilder(s2);
-//
-//        // reverse the whole string using the strRev() function
-//        strRev(builder, 0, builder.length() - 1);
-//
-//        // reverse every word
-//        int n = builder.length();
-//        int start = 0, end = 0;
-//        // Find the start index of each word by detecting spaces.
-//        while (start < n) {
-//            // Find the end index of the word.
-//            while (end < n && builder.charAt(end) != ' ')
-//                ++end;
-//            // Let's call our helper function to reverse the word in-place.
-//            strRev(builder, start, end - 1);
-//            // moving to the next word
-//            start = end + 1;
-//            ++end;
-//        }
-//
-//        return builder.toString();
-//    }
-    public static String reverseWords(String s)
-    {
+    public static String reverseWords(String s) {
         // trim spaces and convert string to string builder
         char[] s1 = s.toCharArray();
-        String s2 = cleanSpaces(s1,s1.length);
+        String s2 = cleanSpaces(s1, s1.length);
         StringBuilder builder = new StringBuilder(s2);
 
         // reverse the whole string using the strRev() function
-        strRev(builder, 0, builder.length() -1);
+        strRev(builder, 0, builder.length() - 1);
 
         // reverse every word
         int n = builder.length();
-        int start = 0, end=0;
-        // find the start index of each word by detecting spaces.
-        while(start<n)
-        {
-            // find the end index of the word.
-            while(end<n && builder.charAt(end) != ' ')
+        int start = 0, end = 0;
+        // Find the start index of each word by detecting spaces.
+        while (start < n) {
+            // Find the end index of the word.
+            while (end < n && builder.charAt(end) != ' ')
                 ++end;
-            // Let's call our helper function to reverse the word in-place
-            strRev(builder, start, end-1);
+            // Let's call our helper function to reverse the word in-place.
+            strRev(builder, start, end - 1);
             // moving to the next word
-            start = end +1;
+            start = end + 1;
             ++end;
         }
+
         return builder.toString();
     }
+
 
     // Function to reverse the whole string
     public static void strRev(StringBuilder sb, int startRev, int endRev) {
@@ -67,20 +41,6 @@ class ReverseWords {
             sb.setCharAt(endRev--, temp);
         }
     }
-//    // trim leading, trailing and multiple spaces
-//    static String cleanSpaces(char[] a, int n) {
-//        // Convert character array to string
-//        String str = new String(a, 0, n);
-//
-//        // Trim extra spaces at the beginning and end of the string
-//        // And replace multiple spaces with a single space
-//        str = str.replaceAll("\\s+", " ").trim();
-//
-//        return str;
-//    }
-    // trim leading, trailing and multiple spaces
-
-
 
     public static void main(String[] args) {
         String[] inputs = {
