0ff7b2cf84b137d12e23ee6aab3dc778d610af06
dev <50889119+andrepared@users.noreply.github.com>
2023-04-08 13:57:02 -0500

    Happy Number

--- src/FastAndSlowPointers/HappyNumbers.java
+++ src/FastAndSlowPointers/HappyNumbers.java
@@ -1,22 +1,48 @@
 package FastAndSlowPointers;
 
-public class HappyNumbers
-{
-public static String printStingWithMarkers(String strn, int pValue)
-{
-    String out = "";
-    for (int i = 0; i < pValue; i++) {
-        out += String.valueOf(strn.charAt(i));
+public class HappyNumbers {
+    public static String printStingWithMarkers(String strn, int pValue) {
+        String out = "";
+        for (int i = 0; i < pValue; i++) {
+            out += String.valueOf(strn.charAt(i));
+        }
+        out += "«";
+        out += String.valueOf(strn.charAt(pValue)) + "»";
+        for (int i = pValue + 1; i < strn.length(); i++) {
+            out += String.valueOf(strn.charAt(i));
+        }
+        return out;
     }
-    out += "«";
-    out += String.valueOf(strn.charAt(pValue)) + "»";
-    for (int i = pValue + 1; i < strn.length(); i++) {
-        out += String.valueOf(strn.charAt(i));
-    }
-    return out;
-}
-public static int sumOfSquaredDigits(int number)
-{
 
+    public static int sumOfSquaredDigits(int number) {
+        int totalSum = 0;
+        System.out.println("\tCalculating the sum of squared digits");
+        System.out.println("\tTotal sum: " + totalSum);
+        int i = 1;
+        while (number > 0) {
+            System.out.println("\tLoop iteration: " + i);
+            int a = String.valueOf(number).length();
+            System.out.println("\t\tNumber; " + number);
+            int digit = number % 10;
+            System.out.println("\t\tWe will start with the last digit of the number " + digit);
+            System.out.println("\t\t" + printStingWithMarkers(String.valueOf(number), a - 1) + " ⟶ Last Digit: " + digit);
+            System.out.println("\t\tUpdating number ⟶ number/10 = " + number + "/10");
+            number = number / 10;
+            System.out.println("\t\t\t\tThe number is now: " + number);
+            System.out.println("\t\t\tTotal sum + square of the digit = " + totalSum + " + " + digit + " * " + digit + " = " + digit);
+            totalSum += (Math.pow(digit, 2));
+            i = i + 1;
+        }
+        return totalSum;
+    }
+
+    public static void main(String args[]) {
+        int[] a = {1, 5, 19, 25, 7};
+//        for (int i = 0; i < a.length; i++) {
+//            System.out.println(i + 1 + "." + "\tInput Number:" + a[i]);
+//            System.out.println("\tSum of squared digits: " + sumOfSquaredDigits(a[i]));
+//            System.out.println(PrintHyphens.repeat("-", 100));
+//        }
+    }
 }
-}
+
