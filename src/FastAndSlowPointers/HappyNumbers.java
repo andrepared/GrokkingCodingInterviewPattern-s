6113cf14c9679d57522685d25847baf459f87af5
dev <50889119+andrepared@users.noreply.github.com>
2023-04-08 14:48:32 -0500

    Just the code

--- src/FastAndSlowPointers/HappyNumbers.java
+++ src/FastAndSlowPointers/HappyNumbers.java
@@ -10,25 +10,16 @@ class HappyNumber {
             number = number / 10;
             totalSum += (Math.pow(digit, 2));
         }
-        System.out.println("\t\tSum of squared digits: " + totalSum);
         return totalSum;
     }
     public static boolean isHappyNumber(int n) {
-        int slowPointer = n; // The slow pointer value
-        System.out.println("\tSetting slow pointer = input number " + slowPointer);
-        System.out.println("\tSetting fast pointer = sum of squared digits of " + n);
-        int fastPointer = sumOfSquaredDigits(n); // The fast pointer value
-        System.out.println("\tFast pointer:" + fastPointer);
-        while (fastPointer != 1 && slowPointer != fastPointer) { // Terminating condition
-            System.out.println("\n\tRepeatedly updating slow and fast pointers\n");
-            // Incrementing the slow pointer by 1 iteration
+        int slowPointer = n;
+        int fastPointer = sumOfSquaredDigits(n);
+
+        while (fastPointer != 1 && slowPointer != fastPointer) {
             slowPointer = sumOfSquaredDigits(slowPointer);
-            System.out.println("\tThe updated slow pointer is " + slowPointer);
-            // Incrementing the fast pointer by 2 iterations
-            fastPointer = sumOfSquaredDigits(sumOfSquaredDigits(fastPointer));
-            System.out.println("\tThe updated fast pointer is " + fastPointer + "\n");
+            fastPointer =  sumOfSquaredDigits(sumOfSquaredDigits(fastPointer));
         }
-        System.out.println("\tIs it a happy number?: " + (fastPointer == 1)); // If 1 is found then it returns True, otherwise False
         return fastPointer == 1;
     }
 
@@ -36,8 +27,10 @@ class HappyNumber {
         int a[] = {1, 5, 19, 25, 7};
         for (int i = 0; i < a.length; i++) {
             System.out.println((i + 1) + ".\tInput Number: " + a[i]);
-            isHappyNumber(a[i]);
-            //
+            String output = isHappyNumber(a[i]) ? "True" : "False";
+
+            System.out.println("\n\tIs it a happy number? " + output);
+//            System.out.println(PrintHyphens.repeat("-", 100));
         }
     }
 }
