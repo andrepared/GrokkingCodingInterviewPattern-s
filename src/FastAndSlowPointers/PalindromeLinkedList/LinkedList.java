3481d4475b6a805eaf66654543b0f953765e34e9
dev <50889119+andrepared@users.noreply.github.com>
2023-04-10 07:51:52 -0500

    Constructing LinkedList class

--- src/FastAndSlowPointers/PalindromeLinkedList/LinkedList.java
+++ src/FastAndSlowPointers/PalindromeLinkedList/LinkedList.java
@@ -1,4 +1,27 @@
 package FastAndSlowPointers.PalindromeLinkedList;
 
-public class LinkedList {
+public class LinkedList<T>
+{
+    public LinkedListNode head;
+    // constructor will be used to create LinkedList object
+    public LinkedList()
+    {
+        this.head = null;
+    }
+    //insertNodeAtHead method will inset a LinkedListNode at head of a Linked List.
+    public void insertAtHead(LinkedListNode node)
+    {
+        if(this.head == null)
+        {
+            this.head = node;
+        }
+        else
+        {
+            node.next = this.head;
+            this.head = node;
+        }
+    }
+    // create LinkedList method will create the linkedlist using the given integer array with the help of
+    // the insertAtHead method.
+
 }
