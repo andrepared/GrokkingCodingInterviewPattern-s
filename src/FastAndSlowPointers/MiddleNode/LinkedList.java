9c16cc3bf74065dd3d93a0f7e53ced2efe007843
dev <50889119+andrepared@users.noreply.github.com>
2023-04-09 23:22:57 -0500

    LinkedList

--- src/FastAndSlowPointers/MiddleNode/LinkedList.java
+++ src/FastAndSlowPointers/MiddleNode/LinkedList.java
@@ -1,22 +1,32 @@
 package FastAndSlowPointers.MiddleNode;
 import java.util.*;
 // template for the linkedlist class
-public class LinkedList <T>
-{
+import java.util.*;
+// Template for the linked list
+class LinkedList<T> {
     public LinkedListNode head;
-    // constructor for linkedist
-
-    public LinkedList()
-    {
+    // constructor will be used to make a LinkedList type object
+    public LinkedList() {
         this.head = null;
     }
-
-    // create method for inseretingAtHead
-    public void insertAtHead(LinkedList node)
-    {
-        if (this.head == null)
-        {
+    // insertNodeAtHead method will insert a LinkedListNode at head
+    // of a linked list.
+    public void insertNodeAtHead(LinkedListNode node) {
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            node.next = this.head;
             this.head = node;
         }
     }
+    // createLinkedList method will create the linked list using the
+    // given integer array with the help of InsertAthead method.
+    public void createLinkedList(int[] lst) {
+        for (int i = lst.length - 1; i >= 0; i--) {
+            LinkedListNode newNode = new LinkedListNode(lst[i]);
+            insertNodeAtHead(newNode);
+        }
+    }
+
+
 }
