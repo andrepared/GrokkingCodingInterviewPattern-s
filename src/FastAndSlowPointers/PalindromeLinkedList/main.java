19bb3b4c3e890626655f4b83268a2d169370724f
dev <50889119+andrepared@users.noreply.github.com>
2023-04-10 08:13:30 -0500

    build helper functions to reverse linked list

--- src/FastAndSlowPointers/PalindromeLinkedList/main.java
+++ src/FastAndSlowPointers/PalindromeLinkedList/main.java
@@ -11,6 +11,42 @@ public class main
         LinkedListNode revertData = null;
         // Traverse linkedlist through fast and slow pointers to
         // get to the middle of the node
+        while(fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+    public static boolean compareTwoHalves(LinkedListNode firstHalf, LinkedListNode secondHalf)
+    {
+        while(firstHalf != null & secondHalf != null)
+        {
+            if(firstHalf.data != secondHalf)
+            {
+                return false;
+            }
+            else
+            {
+                firstHalf = firstHalf.next;
+                secondHalf = secondHalf.next;
+            }
+        }
+        return true;
+    }
+        public static LinkedListNode reversedList(LinkedListNode slowPtr)
+        {
+            LinkedListNode reverse = null;
+            while(slowPtr != null)
+            {
+                LinkedListNode next = slowPtr.next;
+                slowPtr.next = reverse;
+                reverse = slowPtr;
+            }
+            return reverse;
+        }
+
+    public static void main(String[] args) {
 
     }
 }
