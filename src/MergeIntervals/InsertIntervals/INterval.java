fe6e2a86834c744ed0d00438bd421d3d9dae81ee
dev <50889119+andrepared@users.noreply.github.com>
2023-04-10 12:13:39 -0500

    finished constructing interval class

--- src/MergeIntervals/InsertIntervals/INterval.java
+++ src/MergeIntervals/InsertIntervals/INterval.java
@@ -1,9 +1,33 @@
 package MergeIntervals.InsertIntervals;
 
 
+import MergeIntervals.Interval;
+
 import java.util.*;
 
 public class INterval
 {
+    int start;
+    int end;
+    boolean closed;
 
+    public INterval(int start, int end)
+    {
+        this.start = start;
+        this.end = end;
+        this.closed=true;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
+    
 }
