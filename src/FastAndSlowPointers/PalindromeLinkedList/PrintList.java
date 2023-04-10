42723b17829433448edac43d2561de464e7ae371
dev <50889119+andrepared@users.noreply.github.com>
2023-04-10 08:05:02 -0500

    Print List class finished construction

--- src/FastAndSlowPointers/PalindromeLinkedList/PrintList.java
+++ src/FastAndSlowPointers/PalindromeLinkedList/PrintList.java
@@ -1,4 +1,23 @@
 package FastAndSlowPointers.PalindromeLinkedList;
 
-public class PrintList {
+public class PrintList
+{
+    public static void printListWithForwardArrow(LinkedListNode head)
+    {
+        LinkedListNode temp = head;
+
+        while(temp != null)
+        {
+            System.out.println(temp.data); // print node value
+            temp = temp.next;
+            if (temp != null)
+            {
+                System.out.println(" ➡ ");
+            }
+            else
+            {
+                System.out.println(" ➡ null\n");
+            }
+        }
+    }
 }
