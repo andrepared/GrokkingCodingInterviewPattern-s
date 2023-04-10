fad47c9e8627fd92504239f34d7dffe347b1b566
dev <50889119+andrepared@users.noreply.github.com>
2023-04-10 10:28:43 -0500

    Check if list is empty

--- src/MergeIntervals/main.java
+++ src/MergeIntervals/main.java
@@ -1,6 +1,19 @@
 package MergeIntervals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class main
 {
+    public static List<Interval> mergeIntervals(List <Interval> intervals)
+    {
+        List<Interval> result = new ArrayList <Interval> ();
 
+        // if the list is empty
+        if(intervals.size() == 0)
+        {
+            System.out.println("The list is empty");
+            return result;
+        }
+    }
 }
