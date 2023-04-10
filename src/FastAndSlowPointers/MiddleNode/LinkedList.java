55e6aaf2fb846da8215f4438066f551863468976
dev <50889119+andrepared@users.noreply.github.com>
2023-04-09 23:12:43 -0500

    Constructor for LinkedList

--- src/FastAndSlowPointers/MiddleNode/LinkedList.java
+++ src/FastAndSlowPointers/MiddleNode/LinkedList.java
@@ -1,4 +1,13 @@
 package FastAndSlowPointers.MiddleNode;
+import java.util.*;
+// template for the linkedlist class
+public class LinkedList
+{
+    LinkedListNode head;
+    // constructor for linkedist
+    LinkedList(LinkedList head)
+    {
+        this.head = head;
 
-public class LinkedList {
+    }
 }
