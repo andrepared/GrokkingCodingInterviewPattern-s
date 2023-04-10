67be3032b26a95f4278cc9aba24746970cf4f737
dev <50889119+andrepared@users.noreply.github.com>
2023-04-10 08:07:31 -0500

    Constructing Palindrome List class

--- src/FastAndSlowPointers/PalindromeLinkedList/main.java
+++ src/FastAndSlowPointers/PalindromeLinkedList/main.java
@@ -1,5 +1,16 @@
 package FastAndSlowPointers.PalindromeLinkedList;
 
-public class main {
+// class Palindrome List
 
+public class main
+{
+    public static boolean palindrome(LinkedListNode head)
+    {
+        LinkedListNode slow = head;
+        LinkedListNode fast = head;
+        LinkedListNode revertData = null;
+        // Traverse linkedlist through fast and slow pointers to
+        // get to the middle of the node
+
+    }
 }
