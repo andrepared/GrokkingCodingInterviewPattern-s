fc456c88d5a8304cf762af776cc5ae477fb86636
dev <50889119+andrepared@users.noreply.github.com>
2023-04-10 16:55:05 -0500

    Built comparator method in main class

--- src/MergeIntervals/EmployeeFreeTime/main.java
+++ src/MergeIntervals/EmployeeFreeTime/main.java
@@ -1,8 +1,22 @@
 package MergeIntervals.EmployeeFreeTime;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
+class SortById implements Comparator<Interval>
+{
+    // used for sorting in ascending order of ID
+    public int compare(Interval a, Interval b)
+    {
+        return a.getStart() - b.getStart();
+    }
+}
 
 public class main
 {
-
+    public static List< Interval > employeeFreeTime(ArrayList< ArrayList <Interval>> schedule)
+    {
+        
+    }
 }
