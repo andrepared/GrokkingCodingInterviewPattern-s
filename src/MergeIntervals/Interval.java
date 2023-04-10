e737d9c2f4aede0e40a48b0888f498e4c26d6abe
dev <50889119+andrepared@users.noreply.github.com>
2023-04-10 10:23:08 -0500

    Built the helper class Interval

--- src/MergeIntervals/Interval.java
+++ src/MergeIntervals/Interval.java
@@ -1,4 +1,30 @@
 package MergeIntervals;
 
-public class Interval {
+import java.util.*;
+
+public class Interval
+{
+    int start;
+    int end;
+    boolean closed;
+    public Interval(int start, int end)
+    {
+        this.start = start;
+        this.end = end;
+        this.closed = true; // By default, the interval is closed.
+    }
+    public int getStart()
+    {
+        return start;
+    }
+    public int getEnd()
+    {
+        return end;
+    }
+
+    // set the flag for closed/open
+    public void setClosed(boolean closed)
+    {
+        this.closed = closed;
+    }
 }
