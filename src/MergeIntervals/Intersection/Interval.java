4c9aeb5c3d262622d63d21d534550451c5297373
dev <50889119+andrepared@users.noreply.github.com>
2023-04-10 16:38:44 -0500

    Interval class built

--- src/MergeIntervals/Intersection/Interval.java
+++ src/MergeIntervals/Intersection/Interval.java
@@ -1,4 +1,28 @@
 package MergeIntervals.Intersection;
 
-public class Interval {
+public class Interval
+{
+    int start;
+    int end;
+    boolean closed = false; // By default, the interval is closed.
+
+    public Interval(int start, int end)
+    {
+        this.start = start;
+        this.end = end;
+        this.closed = true;
+    }
+    public int getStart()
+    {
+        return start;
+    }
+    public int getEnd()
+    {
+        return end;
+    }
+    // set the the flag for closed/open
+    public void setClosed(boolean closed)
+    {
+        this.closed = closed;
+    }
 }
