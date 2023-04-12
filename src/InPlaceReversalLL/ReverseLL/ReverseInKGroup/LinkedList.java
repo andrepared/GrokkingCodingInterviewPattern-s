53a66887f488aa20035c7a45ea2233bf4c871ee7
dev <50889119+andrepared@users.noreply.github.com>
2023-04-11 23:01:43 -0500

    Completed LinkedList Class

--- src/InPlaceReversalLL/ReverseLL/ReverseInKGroup/LinkedList.java
+++ src/InPlaceReversalLL/ReverseLL/ReverseInKGroup/LinkedList.java
@@ -11,6 +11,26 @@ public class LinkedList
     {
         this.head = null;
     }
-    
+
+    //insertNodeAtHead method
+    public void insertNodeAtHead(LinkedListNode node)
+    {
+        if (this.head == null)
+        {
+            this.head = node;
+        } else
+        {
+            node.next = this.head;
+            this.head = node;
+        }
+    }
+    // create_linked_list method will create the linked list using the
+    // given integer array with the help of InsertAthead method.
+    public void createLinkedList(List<Integer> lst) {
+        for (int i = lst.size() - 1; i >= 0; i--) {
+            LinkedListNode newNode = new LinkedListNode(lst.get(i));
+            insertNodeAtHead(newNode);
+        }
+    }
 
 }
