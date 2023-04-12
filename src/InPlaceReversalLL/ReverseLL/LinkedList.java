0f26e4c49db0d1dfe4579940c653162518683509
dev <50889119+andrepared@users.noreply.github.com>
2023-04-11 22:17:29 -0500

    Constructed LinkedList class

--- src/InPlaceReversalLL/ReverseLL/LinkedList.java
+++ src/InPlaceReversalLL/ReverseLL/LinkedList.java
@@ -1,4 +1,35 @@
 package InPlaceReversalLL.ReverseLL;
 
-public class LinkedList {
+import java.util.List;
+
+public class LinkedList
+{
+    public LinkedListNode head;
+    // Linked List will be used to make a LinkedList type Object
+    public LinkedList()
+    {
+        this.head = null;
+    }
+    //insertNodeAtHead will be helper function
+    public void insertNodeAtHead(LinkedListNode node)
+    {
+        if (this.head == null)
+        {
+            this.head = node;
+        }
+        else {
+            node.next = this.head;
+            this.head = node;
+        }
+    }
+    // create linked_list method will create a linked list using
+    // given integer array with the help of insertAtHead method
+    public void createLinkedList(List<Integer> list)
+    {
+        for(int i=list.size()-1;i>=0;i--)
+        {
+            LinkedListNode newNode = new LinkedListNode(list.get(i));
+            insertNodeAtHead(newNode);
+        }
+    }
 }
