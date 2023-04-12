b9d5a79d6a939fc3f69ddf3f62dc50f927d452ec
dev <50889119+andrepared@users.noreply.github.com>
2023-04-11 22:54:31 -0500

    Built LinkedListNode Class

--- src/InPlaceReversalLL/ReverseLL/ReverseInKGroup/LinkedList.java
+++ src/InPlaceReversalLL/ReverseLL/ReverseInKGroup/LinkedList.java
@@ -2,4 +2,5 @@ package InPlaceReversalLL.ReverseLL.ReverseInKGroup;
 
 public class LinkedList
 {
+    // template for creating LinkedList
 }
--- src/InPlaceReversalLL/ReverseLL/ReverseInKGroup/LinkedListNode.java
+++ src/InPlaceReversalLL/ReverseLL/ReverseInKGroup/LinkedListNode.java
@@ -1,4 +1,13 @@
 package InPlaceReversalLL.ReverseLL.ReverseInKGroup;
 
-public class LinkedListNode {
+public class LinkedListNode
+{
+    public int data;
+    public LinkedListNode next;
+    // Constructor for create LinkedListNode type object.
+    public LinkedListNode(int data)
+    {
+        this.data = data;
+        this.next = next;
+    }
 }
