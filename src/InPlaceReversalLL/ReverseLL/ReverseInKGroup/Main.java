63a4dcb9de2b9fe70f05fa58fbdcce4d0f05f805
dev <50889119+andrepared@users.noreply.github.com>
2023-04-11 23:40:39 -0500

    Full code for reverse k group

--- src/InPlaceReversalLL/ReverseLL/ReverseInKGroup/Main.java
+++ src/InPlaceReversalLL/ReverseLL/ReverseInKGroup/Main.java
@@ -1,8 +1,11 @@
 package InPlaceReversalLL.ReverseLL.ReverseInKGroup;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main
 {
-    public static LinkedListNode reverse(LinkedListNode head)
+    public static List<LinkedListNode> reverse(LinkedListNode head, int k)
     {
         LinkedListNode previous = null;
         LinkedListNode current = head;
@@ -10,7 +13,7 @@ public class Main
         int index = 0;
         while(current != null)
         {
-            index+=1;
+
             // temp store the next node
             next = current.next;
             // reverse the current node
@@ -20,8 +23,74 @@ public class Main
             previous = current;
             // move to next node
             current = next;
+            index+=1;
+        }
+        List<LinkedListNode> resultList = Arrays.asList(previous, current, next);
+        return resultList;
+    }
+    public static int findLength(LinkedListNode start)
+    {
+        LinkedListNode current = start;
+        int count =0;
+        while(current != null)
+        {
+            current = current.next;
+            count +=1;
 
         }
-        return previous;
+        return count;
+    }
+    public static LinkedListNode reversedLinkedList(LinkedListNode head, int k)
+    {
+        if(k<=1 || head == null)
+            return head;
+
+        int i=0;
+        int count =0;
+        LinkedListNode current = head;
+        LinkedListNode previous = null;
+        LinkedListNode lastNodeOfPreviousPart = null;
+        LinkedListNode lastNodeOfCurrentPart = null;
+        int totalLength = findLength(head);
+        while(true)
+        {
+            i+=1;
+            lastNodeOfPreviousPart = previous;
+            lastNodeOfCurrentPart = current;
+            LinkedListNode next = null;
+            List<LinkedListNode> res = reverse(lastNodeOfCurrentPart, k);
+            previous = res.get(0);
+            current = res.get(1);
+            next = res.get(2);
+            count += k;
+            if(lastNodeOfPreviousPart != null)
+            {
+                lastNodeOfPreviousPart.next = previous;
+            }
+            else
+            {
+                head = previous;
+
+            }
+            lastNodeOfCurrentPart.next = current;
+            if (current == null || (totalLength - count) < k)
+            {
+                break;
+            }
+            previous = lastNodeOfCurrentPart
+
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        LinkedList inputLinkedList = new LinkedList();
+        inputLinkedList.createLinkedList(inputList);
+        System.out.print("The original linked list: ");
+        PrintList.printListWithForwardArrow(inputLinkedList.head);
+        LinkedListNode result = reverseLinkedList(inputLinkedList.head, 3);
+        System.out.print("\nReversed linked list, with k = " + 3 + ": ");
+        PrintList.printListWithForwardArrow(result);
     }
 }
