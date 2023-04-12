1d390f0c0254218b7646fdbfb8821adef64472c2
dev <50889119+andrepared@users.noreply.github.com>
2023-04-11 23:06:34 -0500

    Added LinkedListNode reverse function

--- src/InPlaceReversalLL/ReverseLL/ReverseInKGroup/Main.java
+++ src/InPlaceReversalLL/ReverseLL/ReverseInKGroup/Main.java
@@ -2,4 +2,26 @@ package InPlaceReversalLL.ReverseLL.ReverseInKGroup;
 
 public class Main
 {
+    public static LinkedListNode reverse(LinkedListNode head)
+    {
+        LinkedListNode previous = null;
+        LinkedListNode current = head;
+        LinkedListNode next = null;
+        int index = 0;
+        while(current != null)
+        {
+            index+=1;
+            // temp store the next node
+            next = current.next;
+            // reverse the current node
+            current.next = previous;
+            // before we move to the next node, point prev
+            // to curr node.
+            previous = current;
+            // move to next node
+            current = next;
+
+        }
+        return previous;
+    }
 }
