9bcf1193977fa806e8c72f3a50f022a4049f3c7c
dev <50889119+andrepared@users.noreply.github.com>
2023-04-11 22:56:08 -0500

    Constructing LinkedList class constructor

--- src/InPlaceReversalLL/ReverseLL/ReverseInKGroup/LinkedList.java
+++ src/InPlaceReversalLL/ReverseLL/ReverseInKGroup/LinkedList.java
@@ -1,6 +1,16 @@
 package InPlaceReversalLL.ReverseLL.ReverseInKGroup;
 
+import java.util.*;
+
 public class LinkedList
 {
     // template for creating LinkedList
+    public LinkedListNode head;
+
+    public LinkedList ()
+    {
+        this.head = null;
+    }
+    
+
 }
