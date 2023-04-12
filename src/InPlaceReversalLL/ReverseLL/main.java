0ed98543cb65b13f9b36116b40cbe9f356a58c5f
dev <50889119+andrepared@users.noreply.github.com>
2023-04-11 22:27:31 -0500

    Class for reversed LinkedList

--- src/InPlaceReversalLL/ReverseLL/main.java
+++ src/InPlaceReversalLL/ReverseLL/main.java
@@ -1,4 +1,40 @@
 package InPlaceReversalLL.ReverseLL;
 
-public class main {
+import java.util.List;
+import java.util.Arrays;
+
+public class main
+{
+    public static LinkedListNode reverse(LinkedListNode head)
+    {
+        if (head == null || head.next == null)
+        {
+            return head;
+        }
+        LinkedListNode listToDo = head.next;
+        LinkedListNode reversedList = head;
+        reversedList.next = null;
+        while(listToDo != null)
+        {
+            LinkedListNode temp = listToDo;
+            listToDo = listToDo.next;
+            temp.next = reversedList;
+            reversedList = temp;
+        }
+        return reversedList;
+    }
+
+    public static void main(String[] args) {
+        // Declaring and creating a linked list
+        LinkedList obj1 = new LinkedList();
+        List<Integer> inputList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        // Creating Linked Lists
+        obj1.createLinkedList(inputList);
+        // Displaying original linked list
+        System.out.print("The original linked list: ");
+        PrintList.printListWithForwardArrow(obj1.head);
+        LinkedListNode result = reverse(obj1.head);
+        System.out.print("\n\nThe reversed linked list:  ");
+        PrintList.printListWithForwardArrow(result);
+    }
 }
