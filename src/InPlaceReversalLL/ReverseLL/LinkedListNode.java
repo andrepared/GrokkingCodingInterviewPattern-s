69c33587cbaa12810d60deab05c8780a09c977f1
dev <50889119+andrepared@users.noreply.github.com>
2023-04-11 22:11:56 -0500

    In-place reversal of a linked list

--- src/InPlaceReversalLL/ReverseLL/LinkedListNode.java
+++ src/InPlaceReversalLL/ReverseLL/LinkedListNode.java
@@ -1,4 +1,13 @@
 package InPlaceReversalLL.ReverseLL;
 
-public class LinkedListNode {
+public class LinkedListNode
+{
+    public int data;
+    public LinkedListNode next;
+
+    public LinkedListNode(int data)
+    {
+        this.data = data;
+        this.next = next;
+    }
 }
