57abbe62c3ff67e3d356eed90f1714386a5f1a40
dev <50889119+andrepared@users.noreply.github.com>
2023-04-11 20:58:18 -0500

    Employee Free Time

--- src/MergeIntervals/EmployeeFreeTime/main.java
+++ src/MergeIntervals/EmployeeFreeTime/main.java
@@ -1,9 +1,6 @@
 package MergeIntervals.EmployeeFreeTime;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 class SortById implements Comparator<Interval>
 {
@@ -43,6 +40,21 @@ public class main {
     }
 
     public static void main(String[] args) {
-        
+        List < List < List < Interval >>> inputs1 = (List < List < List < Interval >>>)Arrays.asList(Arrays.asList(Arrays.asList(new Interval(1, 2), new Interval(5, 6)), Arrays.asList(new Interval(1, 3)), Arrays.asList(new Interval(4, 10))), Arrays.asList(Arrays.asList(new Interval(1, 3), new Interval(6, 7)), Arrays.asList(new Interval(2, 4)), Arrays.asList(new Interval(2, 5), new Interval(9, 12))), Arrays.asList(Arrays.asList(new Interval(2, 3), new Interval(7, 9)), Arrays.asList(new Interval(1, 4), new Interval(6, 7))), Arrays.asList(Arrays.asList(new Interval(3, 5), new Interval(8, 10)), Arrays.asList(new Interval(4, 6), new Interval(9, 12)), Arrays.asList(new Interval(5, 6), new Interval(8, 10))), Arrays.asList(Arrays.asList(new Interval(1, 3), new Interval(6, 9), new Interval(10, 11)), Arrays.asList(new Interval(3, 4), new Interval(7, 12)), Arrays.asList(new Interval(1, 3), new Interval(7, 10)), Arrays.asList(new Interval(1, 4)), Arrays.asList(new Interval(7, 10), new Interval(11, 12))), Arrays.asList(Arrays.asList(new Interval(1, 2), new Interval(3, 4), new Interval(5, 6), new Interval(7, 8)), Arrays.asList(new Interval(2, 3), new Interval(4, 5), new Interval(6, 8))), Arrays.asList(Arrays.asList(new Interval(1, 2), new Interval(3, 4), new Interval(5, 6), new Interval(7, 8), new Interval(9, 10), new Interval(11, 12)), Arrays.asList(new Interval(1, 2), new Interval(3, 4), new Interval(5, 6), new Interval(7, 8), new Interval(9, 10), new Interval(11, 12)), Arrays.asList(new Interval(1, 2), new Interval(3, 4), new Interval(5, 6), new Interval(7, 8), new Interval(9, 10), new Interval(11, 12)), Arrays.asList(new Interval(1, 2), new Interval(3, 4), new Interval(5, 6), new Interval(7, 8), new Interval(9, 10), new Interval(11, 12))));
+        int i = 1;
+
+        ArrayList<ArrayList<ArrayList<Interval>>> inputs = new ArrayList<ArrayList<ArrayList<Interval>>>();
+        for(int j = 0; j < inputs1.size(); j++)
+        {
+            inputs.add(new ArrayList<ArrayList<Interval>>());
+            for(int k = 0; k < inputs1.get(j).size(); k++)
+            {
+                inputs.get(j).add(new ArrayList<Interval>());
+                for(int g = 0; g < inputs1.get(j).get(k).size(); g++)
+                {
+                    inputs.get(j).get(k).add(inputs1.get(j).get(k).get(g));
+                }
+            }
+        }
     }
 }
