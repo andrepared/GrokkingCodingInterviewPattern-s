60deb261185ec3f7b6367ea06a4239f4c3e8d747
dev <50889119+andrepared@users.noreply.github.com>
2023-04-11 22:21:08 -0500

    Constructed PrintList class

--- src/InPlaceReversalLL/ReverseLL/PrintList.java
+++ src/InPlaceReversalLL/ReverseLL/PrintList.java
@@ -1,4 +1,21 @@
 package InPlaceReversalLL.ReverseLL;
 
-public class PrintList {
+public class PrintList
+{
+    public static void printListWithForwardArrow(LinkedListNode head)
+    {
+        LinkedListNode temp = head;
+
+        while(temp != null)
+        {
+            System.out.println(temp.data);
+            temp = temp.next;
+            if (temp != null)
+            {
+                System.out.println("➡");
+            }
+        }
+        // if this is the last node, print null at the end
+        System.out.println("→ null");
+    }
 }
