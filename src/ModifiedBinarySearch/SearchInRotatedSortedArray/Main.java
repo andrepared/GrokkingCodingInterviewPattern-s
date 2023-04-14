45b96a22e1feb0b4477021eb75742078063aa678
dev <50889119+andrepared@users.noreply.github.com>
2023-04-13 19:31:24 -0500

    Modified Binary Search

---
+++ src/ModifiedBinarySearch/SearchInRotatedSortedArray/Main.java
@@ -0,0 +1,22 @@
+package ModifiedBinarySearch.SearchInRotatedSortedArray;
+
+import java.util.List;
+
+public class Main
+{
+    public static int binarySearch (List<Integer> nums, int target)
+    {
+        int start = 0;
+        int end = nums.size() -1;
+        int mid = 0;
+        if(start > end) return -1;
+
+        while(start<=end)
+        {
+            // Finding the mid, using integer division.
+            mid = start + (end -start) / 2;
+            // Targer value is present at the middle of the array.
+
+        }
+    }
+}
